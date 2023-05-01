efed35850616c5eba7a0fd236fcd22ee5ecb28d5
Syed Mohammad Yasir <smy329@gmail.com>
2023-05-02 03:04:31 +0600

    chefsData and recipesData collected

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const chefsData = require('./data/chefs.json');
+const recipesData = require('./data/recipes.json');
+
 const app = express();
 app.use(cors());
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Chef Recipe is going on');
+});
+
+app.get('/chefs', (req, res) => {
+  res.send(chefsData);
+});
+
+app.get('/recipes', (req, res) => {
+  res.send(recipesData);
 });
 
 app.listen(port, () => {
